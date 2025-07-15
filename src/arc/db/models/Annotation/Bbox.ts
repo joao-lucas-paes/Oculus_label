265b5478718ef4d55e5fc8627d8483208fcf5154
Joao Lucas Paes <joaozinhopaes12@gmail.com>
2025-07-15 14:03:06 -0400

    fixing inconsistencies relative to schema and implementation

--- src/arc/db/models/Annotation/Bbox.ts
+++ src/arc/db/models/Annotation/Bbox.ts
@@ -1,17 +1,14 @@
-import { Data } from '../Data';
 import Annotation from "./Annotation";
-import { NULL_STRING } from "../../../../const/arc";
-import Classes from '../Classes';
 
-export default class BBox extends Annotation {
+export default class BBox {
+    private annotation_info: Annotation; // annotation information
     private x_min: number; // left coordinate of the bounding box
     private y_min: number; // top coordinate of the bounding box
     private x_max: number; // width of the bounding box
     private y_max: number; // height of the bounding box
 
-    constructor(annotation_id:number, data_id:Data, classes:Classes, caption:string=NULL_STRING,
-        x_min:number, y_min:number, x_max:number, y_max:number) {
-        super(annotation_id,data_id,classes,caption);
+    constructor(annotation:Annotation, x_min:number, y_min:number, x_max:number, y_max:number) {
+        this.annotation_info = annotation;
         this.x_min = x_min;
         this.y_min = y_min;
         this.x_max = x_max;
--- src/arc/db/models/Annotation/Mask.ts
+++ src/arc/db/models/Annotation/Mask.ts
@@ -1,14 +1,12 @@
-import { Data } from '../Data';
 import Annotation from "./Annotation";
-import { NULL_STRING } from "../../../../const/arc";
 import tuple from '../../../interface/tuple';
-import Classes from '../Classes';
 
-export default class Mask extends Annotation {
+export default class Mask  {
+    private annotation_info: Annotation;
     private mask: Array<tuple> = new Array<tuple>();
 
-    constructor(annotation_id:number, data_id:Data, classes:Classes, caption:string=NULL_STRING, mask:object) {
-        super(annotation_id,data_id,classes,caption);
+    constructor(annotation:Annotation, mask:object) {
+        this.annotation_info = annotation;
         this.mask = Array.from(Object.values(mask));
     }
 };
