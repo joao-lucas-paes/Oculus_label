4cce2b278ead44384704b0b89db6f6688f44f385
Joao Lucas Paes <joaozinhopaes12@gmail.com>
2025-07-15 16:52:52 -0400

    fixing bad statement of project_id from Project to number.

--- src/arc/interface/Data.ts
+++ src/arc/interface/Data.ts
@@ -2,5 +2,5 @@ export default interface Data {
     data_id: number; // data id
     name: string; // file name
     frame_index: number; // index of the frame in a video, -1 means not a video
-    project: Number; // project that the data belongs to
+    project_id: Number; // project that the data belongs to
 }
