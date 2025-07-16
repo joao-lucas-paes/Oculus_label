d7e176c09a5c0b671150e046bcf600a03a20f402
Joao Lucas Paes <joaozinhopaes12@gmail.com>
2025-07-15 20:17:34 -0400

    fix class_id statement name (class_id->id)

--- src/arc/interface/Classes.ts
+++ src/arc/interface/Classes.ts
@@ -1,8 +1,8 @@
 export default interface Classes {
-    class_id: number; // Class id
+    id: number; // Class id
+    project_id: number; // id project that the class belongs to
     name: string; // class name
     description: string; // class description
-    project_id: number; // id project that the class belongs to
     r: number; // red
     g: number; // green
     b: number; // blue
