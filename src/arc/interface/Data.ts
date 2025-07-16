a6c7a617ecb162b97fddd2bb5ca84fee714a2551
Joao Lucas Paes <joaozinhopaes12@gmail.com>
2025-07-15 20:15:59 -0400

    fix id statement of Data interface

--- src/arc/interface/Data.ts
+++ src/arc/interface/Data.ts
@@ -1,5 +1,5 @@
 export default interface Data {
-    data_id: number; // data id
+    id: number; // data id
     name: string; // file name
     frame_index: number; // index of the frame in a video, -1 means not a video
     project_id: Number; // project that the data belongs to
